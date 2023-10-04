1bed7760ba3d4dbb683333da1e8acb3171ce7a1f
mateus-damasceno-zup <mateus.damasceno@zup.com.br>
2023-10-04 14:52:42 -0300

    criado classe CidadesModel e CidadesRepository

---
+++ src/main/java/com/catalisa/cidadesegura/domain/repository/CidadesRepository.java
@@ -0,0 +1,8 @@
+package com.catalisa.cidadesegura.domain.repository;
+
+import com.catalisa.cidadesegura.domain.model.CidadesModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+@Repository
+public interface CidadesRepository extends JpaRepository<CidadesModel,Long> {
+}
