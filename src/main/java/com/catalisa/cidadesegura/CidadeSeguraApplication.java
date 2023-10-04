dc966d48eabc58ecb18843057b8b22e80be11f64
meire-zup <jucemeire.lopes@zup.com.br>
2023-10-04 12:52:51 -0300

    classe localidade

---
+++ src/main/java/com/catalisa/cidadesegura/CidadeSeguraApplication.java
@@ -0,0 +1,13 @@
+package com.catalisa.cidadesegura;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CidadeSeguraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CidadeSeguraApplication.class, args);
+	}
+
+}
