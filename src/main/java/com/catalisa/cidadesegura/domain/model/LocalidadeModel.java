401d5f37988db85f1765808fee319fc067cfa985
meire-zup <jucemeire.lopes@zup.com.br>
2023-10-05 12:55:42 -0300

    classe LocalidadeModel

--- src/main/java/com/catalisa/cidadesegura/domain/model/LocalidadeModel.java
+++ src/main/java/com/catalisa/cidadesegura/domain/model/LocalidadeModel.java
@@ -1,1 +1,27 @@
+package com.catalisa.cidadesegura.domain.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "Estados")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class LocalidadeModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idEstado;
+
+    private String ruaLocalidade;
+    private Integer numeroLocalidade;
+    private String bairroLocalidade;
+    private String pontoReferenciaLocalidade;
+    @ManyToOne
+    @JoinColumn(name = "cidade_id")
+    private CidadesModel cidadesModel;
+
+}
